78ed31fe3b92c3de221fbed72728e8caeba6087c
Park SY <parksy@pal-nat186-19-210.itap.purdue.edu>
2025-01-23 16:30:20 -0500

    Lab 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,47 @@ import "./App.css";
 import "./styles/reset.css";
 import About from "./Components/About.jsx";
 import Navbar from "./Components/Navbar.jsx";
-import Card1 from "./Components/Card1.jsx";
-import Card2 from "./Components/Card2.jsx";
+import image_man from "./assets/boy-picture.jpg";
+import image_woman from "./assets/girl-cartoon.jpg";
+import Card from "./Components/Card.jsx";
 
 const App =() => {
+  const profiles= [
+    {
+      img: image_man,
+      name: 'John Doe',
+      title:'Software Engineer',
+      email:'a@a.com'
+    },
+    {
+      img: image_woman,
+      name: 'Lily Smith',
+      title:'Software Enginner',
+      email:'b@b.com'
+    }
+  ]
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main>
-        <div className ="section">
-          <div className= "container">
-            <h1>Profile App</h1>
+        <Wrapper>
+          <h1>Profile App</h1>
+        </Wrapper>
+        <Wrapper>
+          <About />
+        </Wrapper>
+        <Wrapper>
+          <div className="profile-cards">
+            {profiles.map((profile) => (
+              <Card key={profile.email} {...profile} />
+            ))}
           </div>
-        </div>
-        <div className = "section">
-          <div className="container">
-            <About />
-          </div>
-          </div>
-        <div className ="section">
-          <div className ="container">
-            <div className ="profile-cards">
-              <Card1 />
-              <Card2 />
-            </div>
-          </div>
-        </div>
+        </Wrapper>
       </main>
     </>
   );
 }
+
 export default App;
