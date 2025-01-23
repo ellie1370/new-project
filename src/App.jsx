8e90683aa8038c98507d10d1d912c61b20fce10f
Park SY <parksy@pal-nat186-19-210.itap.purdue.edu>
2025-01-23 17:04:31 -0500

    Lab 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from "./Components/Navbar.jsx";
 import image_man from "./assets/boy-picture.jpg";
 import image_woman from "./assets/girl-cartoon.jpg";
 import Card from "./Components/Card.jsx";
+import Wrapper from "./Components/Wrapper.jsx";
+import {useState } from 'react';
 
 const App =() => {
   const profiles= [
@@ -21,6 +23,11 @@ const App =() => {
       email:'b@b.com'
     }
   ]
+  const [clicked, setClicked] = useState(false);
+  const handleClick =() => {
+    setClicked(!clicked); 
+  }
+  
   return (
     <>
       <header>
@@ -29,6 +36,8 @@ const App =() => {
       <main>
         <Wrapper>
           <h1>Profile App</h1>
+          <button onClick={handleClick}>
+            {clicked ? "Click me" : "Clicked"}</button>
         </Wrapper>
         <Wrapper>
           <About />
